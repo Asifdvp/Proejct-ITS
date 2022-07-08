e6269c3a89d69a57b75da79b57f7719884d4b35c
Asifdvp <asif.mustafayev.13@gmail.com>
2022-07-08 12:28:25 +0400

    textField

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import TextInput from './Components/Inputs/TextInput';
-import './App.css'
+import './App.css';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
-
-     <TextInput/>
+<Grid container spacing={2}>
+  <Grid item xs={4}>
+    <TextInput  placeholder="Layihənin adı" label="Layihənin adı"/>
+  </Grid>
+  <Grid item xs={4}>
+    <TextInput   placeholder="Layihənin kodu" label="Layihənin kodu"/>
+  </Grid>
+  <Grid item xs={4}>
+    <TextInput  placeholder="Layihənin adı" label="Layihənin adı" />
+  </Grid>
+  <Grid item xs={12}>
+    <TextInput  placeholder="Layihənin adı" label="Layihənin adı" />
+  </Grid>
+</Grid>
+   
     </div>
   );
 }
--- src/Components/Inputs/TextInput.tsx
+++ src/Components/Inputs/TextInput.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import {  styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
-import './style.css'
+import './style.css';
 const CssTextField = styled(TextField)({
+
   //focus olanda label rengi
   '& label.Mui-focused': {
     color: '#292D32',
@@ -11,15 +12,21 @@ const CssTextField = styled(TextField)({
   // '& .MuiInput-underline:after': {
   //   borderBottomColor: 'green',
   // },
+  ' & .MuiOutlinedInput-input, .MuiInputBase-input':{
+    borderWidth:"0.4px",
+    height:"auto",
+  padding:"16px"
+  },
   '& .MuiOutlinedInput-root': {
-   
     '& fieldset': {
+
       borderColor: '#0F3673',
     },
-    '&:hover fieldset': {
-      borderColor: '#0F3673',
-    },
+    // '&:hover fieldset': {
+    //   borderColor: '#0F3673',
+    // },
     '&.Mui-focused fieldset': {
+    
       borderColor: '#0F3673',
     },
     
@@ -27,13 +34,23 @@ const CssTextField = styled(TextField)({
 });
 
 
-export default function TextInput() {
+ const TextInput:React.FC<{placeholder:any,label:any}> = ({placeholder,label}) =>{
   return (
+  
       <CssTextField 
-      sx={{margin:"50px 0","label":{color:"#292D32",opacity:"0.4"}}}
-      className='text'  
-    label="Custom CSS"  />
+  
+      sx={{
+        padding:"0px",
+        height:"48px",
+        "label":{color:"#292D32",opacity:"0.4"},
      
-   
+      }}
+      className='text'  
+      fullWidth
+      placeholder={placeholder}
+    label={label} />
+     
+  
   );
 }
+export default TextInput
