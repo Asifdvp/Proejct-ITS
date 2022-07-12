807d0862dd73a8a694979ad8054c2f890ddac962
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-12 12:11:37 +0400

    DatePicker

--- src/Components/Inputs/DateInput.tsx
+++ src/Components/Inputs/DateInput.tsx
@@ -5,28 +5,28 @@ import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import * as startOfDay from "date-fns";
+import Calendar from "../../Assets/images/Calendar";
 
-import calendar from "./calendar.svg";
 export default function DateInput() {
-  const calendar = require("./calendar.svg") as any;
+ 
   const [value, setValue] = React.useState<Date | null>(new Date());
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Stack component="form" noValidate spacing={3}>
-        <DesktopDatePicker
+    <LocalizationProvider dateAdapter={AdapterDateFns} >
+        <DesktopDatePicker 
+         
           label="Başlama Tarixi"
           inputFormat="dd.MM.yy"
+      
           value={value}
           minDate={new Date("2017-01-01")}
           components={{
-            OpenPickerIcon: calendar
+            OpenPickerIcon: Calendar
           }}
           onChange={(newValue) => {
             setValue(newValue);
           }}
           renderInput={(params) => <TextField {...params} />}
         />
-      </Stack>
     </LocalizationProvider>
   );
 }
