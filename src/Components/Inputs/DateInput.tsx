34314db2fa59d017ed29f65180c8fb2b1e3ad692
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-12 09:45:45 +0400

    problem svg icon

--- src/Components/Inputs/DateInput.tsx
+++ src/Components/Inputs/DateInput.tsx
@@ -1,32 +1,32 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import * as startOfDay from "date-fns";
 
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import calendar from "./calendar.svg";
 export default function DateInput() {
+  const calendar = require("./calendar.svg") as any;
   const [value, setValue] = React.useState<Date | null>(new Date());
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-    <Stack component="form" noValidate spacing={3}>
-  <DesktopDatePicker
+      <Stack component="form" noValidate spacing={3}>
+        <DesktopDatePicker
           label="Başlama Tarixi"
           inputFormat="dd.MM.yy"
           value={value}
-          minDate={new Date('2017-01-01')}
+          minDate={new Date("2017-01-01")}
           components={{
-            OpenPickerIcon: CalendarTodayIcon 
+            OpenPickerIcon: calendar
           }}
           onChange={(newValue) => {
             setValue(newValue);
           }}
           renderInput={(params) => <TextField {...params} />}
         />
-  
-    </Stack>
+      </Stack>
     </LocalizationProvider>
   );
 }
---
+++ src/Components/Inputs/index.d.ts
@@ -0,0 +1,5 @@
+
+declare module "*/calendar.svg" {
+    const content: any;
+    export default content;
+  }
