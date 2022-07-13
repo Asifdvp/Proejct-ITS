45cd706180c5cc813b781af53f8fe13902f7bcd5
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-13 12:50:12 +0400

    status

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,39 @@ import './App.css';
 import Grid from '@mui/material/Grid';
 import TextArea from './Components/Inputs/TextArea';
 import DateInput from './Components/Inputs/DateInput';
-import SelectInput from './Components/Inputs/SelctInput';
+import SelectInput from './Components/Inputs/SelectInput';
 import CheckSelect from './Components/Inputs/CheckSelect';
+import StatusInput from './Components/Inputs/StatusInput';
 function App() {
   return (
     <div className="App">
 <Grid container spacing={1.5} columns={12}>
 
- <Grid item xs={6}>
+ <Grid item xs={3}>
    <TextArea   placeholder="Layihənin adı" label="Layihənin adı" limit={5000}/>
   </Grid> 
  <Grid item xs={3}>
    <DateInput />
   </Grid> 
- <Grid item xs={6}>
+ <Grid item xs={3}>
    <TextInput  disAble={false}  placeholder="Layihənin adı" label="Layihənin adı"   />
   </Grid> 
- <Grid item xs={6}>
+ <Grid item xs={3}>
    <TextArea  limit={1200} placeholder="Layihənin adı" label="Layihənin adı"   />
   </Grid> 
- <Grid item xs={6}>
+ <Grid item xs={3}>
    <TextInput  disAble={true}  placeholder="Layihənin adı" label="Layihənin adı" />
   </Grid>  
 
 
-  <Grid item xs={6}>
+  <Grid item xs={3}>
  <SelectInput/>
   </Grid>
-  <Grid item xs={6}>
+  <Grid item xs={3}>
  <CheckSelect/>
+  </Grid>
+  <Grid item xs={3}>
+ <StatusInput/>
   </Grid>
 
 
---
+++ src/Components/Inputs/StatusInput.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Arrow from '../../Assets/images/Arrow';
+
+export default function StatusInput() {
+  return (
+    <Autocomplete
+      disablePortal
+      fullWidth
+      id="combo-box-demo"
+      options={top100Films}
+     popupIcon={<Arrow/>}
+      renderInput={(params) => <TextField {...params} label="Movie" />}
+    />
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+
+ 
+  { label: 'Monty Python and the Holy Grail', year: 1975 }
+];
