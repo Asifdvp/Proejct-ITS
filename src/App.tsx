dbef5edea181732e1e55554549f481fe2e57bc84
Ramil <ramilqenberli5@gmail.com>
2022-07-14 12:18:32 +0400

    f

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <CreateTeamTable /> */}
-      {/* <LayiheDetallari /> */}
-      <ModalCreatTeamTable />
+      <LayiheDetallari />
+      {/* <ModalCreatTeamTable /> */}
     </div>
   );
 }
