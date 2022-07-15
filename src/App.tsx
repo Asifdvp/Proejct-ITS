8b2eae475df8686448b5671a529f7cef621735d6
Ramil <ramilqenberli5@gmail.com>
2022-07-15 12:20:52 +0400

    new comit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import CreateTeamTable from './components/Table/CreateTeam';
 import LayiheDetallari from './components/Table/layiheDetallari';
 import ModalCreatTeamTable from './components/Table/ModalCreatTeamTable';
-
+import Card from './components/Card';
 function App() {
   return (
     <div className="App">
       {/* <CreateTeamTable /> */}
-      <LayiheDetallari />
+      {/* <LayiheDetallari /> */}
       {/* <ModalCreatTeamTable /> */}
+      <Card basliq='salam' buttonExist='true'/>
     </div>
   );
 }
