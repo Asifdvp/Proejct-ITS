a569d0af9931f0c1de4cafec73ab98cc44871e52
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-16 23:42:48 +0400

    Modal Component Completed

--- src/Components/Modals/RemoveMemberAlert.tsx
+++ src/Components/Modals/RemoveMemberAlert.tsx
@@ -1,33 +1,26 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import ModalComponent from './ModalComponent';
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import ModalComponent from "./ModalComponent";
+import "./style.css";
 
 export default function RemoveMemberAlert() {
   const [open, setOpen] = React.useState(false);
- const handleOpen = () => setOpen(true);
-   const handleClose = () => setOpen(false);
-
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  React.useEffect(() => {
+    console.log(handleClose);
+  });
   return (
-    
     <div>
-      <Button onClick={(handleOpen)}>Open modal</Button>
-   <ModalComponent open={open} handleClose={handleClose} clear={true}>
-           <Typography>dsfsfsd</Typography>
-   </ModalComponent>
+      <Button onClick={handleOpen}>Open modal</Button>
+
+      <ModalComponent open={open} handleClose={handleClose} clear={true}>
+        <div className="remove-member-alert">
+          <Typography>dsfsfsd</Typography>
+        </div>
+      </ModalComponent>
     </div>
   );
 }
