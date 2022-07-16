03a3dbc17dbc8a5e4e6e2fa7ebc922c2a865b316
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-16 21:55:35 +0400

    pull master

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,12 @@
 import React from 'react';
 import './App.css';
-import CheckSelect from './Components/Inputs/CheckSelect';
-import SelectInput from './Components/Inputs/SelectInput';
-import DateInput from './Components/Inputs/DateInput';
-import TextInput from './Components/Inputs/TextInput';
-import TextArea from './Components/Inputs/TextArea';
-import StatusInput from './Components/Inputs/StatusInput';
-import ClearIcon from '@mui/icons-material/Clear';
-import { Button, Fab, Grid } from '@mui/material';
+import ModalComponent from './Components/Modals/ModalComponent';
 import RemoveMemberAlert from './Components/Modals/RemoveMemberAlert';
-import ContainedButton from './Components/Buttons/ContainedButton';
-import AddIcon from '@mui/icons-material/Add';
 
-import ClearButton from './Components/Buttons/ClearButton';
-import AddButton from './Components/Buttons/AddButton';
-import Elave from './Components/Buttons/AddTextButton';
-import AddTextButton from './Components/Buttons/AddTextButton';
 function App() {
   return (
     <div className="App">
-{/* <RemoveMemberAlert/> */}
-
-{/* <Elave/>
-
-<OutlinedButtons/>
- */}
-<AddButton/>
-<ClearButton/>
-<ContainedButton
-         text={"Ləğv Et"}     
-        paddingSize={"16px 55px"}
-        variantName={"outlined"}
-        disabled={false}
-      />
-
-<ContainedButton
-         text={"Yadda Saxla"}
-        paddingSize={"16px 24px"}
-        variantName={"contain"}
-        disabled={false}
-      />
-
-<ContainedButton
-         text={"Yeni layihe yarat"}
-        paddingSize={"16px 32px"}
-        variantName={"contain"}
-        disabled={false}
-      />
-<ContainedButton
-         text={"Yadda Saxla"}
-        paddingSize={"16px 24px"}
-        variantName={"contain"}
-        disabled={true}
-      />
-<ContainedButton
-         text={"Sil"}
-        paddingSize={"16px 73px"}
-        variantName={"outlined"}
-        disabled={false}
-      />
-<AddTextButton/>
+<RemoveMemberAlert/>
 </div>
 
 )};
--- src/Components/Modals/ModalComponent.tsx
+++ src/Components/Modals/ModalComponent.tsx
@@ -16,7 +16,7 @@ const style = {
   p: 4,
 };
 
-export default function ModalComponent() {
+export default function BasicModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -31,7 +31,12 @@ export default function ModalComponent() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-  {/* {props.children} */}
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
         </Box>
       </Modal>
     </div>
