a91e3ac3401e24eebe355696dfa288443010c42f
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-17 00:42:42 +0400

    RemoveMemberAlertModel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import RemoveMemberAlert from './Components/Modals/RemoveMemberAlert';
+import CreateTeamTable from './Components/Table/CreateTeam';
+import LayiheDetallariTable from './Components/Table/layiheDetallari';
+import ModalCreatTeamTable from './Components/Table/ModalCreatTeamTable';
 function App() {
   return (
     <div className="App">
 <RemoveMemberAlert/>
-
+{/* <CreateTeamTable/>
+<LayiheDetallariTable/> */}
+{/* <ModalCreatTeamTable/> */}
 </div>
 
 )};
--- src/Components/Modals/RemoveMemberAlert.tsx
+++ src/Components/Modals/RemoveMemberAlert.tsx
@@ -4,21 +4,20 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ModalComponent from "./ModalComponent";
 import "./style.css";
-
+import ContainedButton from "../Buttons/ContainedButton";
 export default function RemoveMemberAlert() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  React.useEffect(() => {
-    console.log(handleClose);
-  });
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
 
       <ModalComponent open={open} handleClose={handleClose} clear={true}>
         <div className="remove-member-alert">
-          <Typography>dsfsfsd</Typography>
+        <Typography variant="h2" gutterBottom component="div" >
+        Komanda üzvünü silmək istədiyinizə əminsiniz mi ? </Typography>
+<ContainedButton  text={"sil"} variantName={"contain"} disabled={false} paddingSize={"16px 50.5px"} />
         </div>
       </ModalComponent>
     </div>
