ccb1f2f2ef9ced6336955d631cc10994ef2bac0b
Ramil <ramilqenberli5@gmail.com>
2022-07-18 09:39:50 +0400

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import CreateTeamTable from './components/Table/CreateTeam';
 import LayiheDetallari from './components/Table/layiheDetallari';
 import ModalCreatTeamTable from './components/Table/ModalCreatTeamTable';
 import Card from './components/Card';
+
 function App() {
   return (
     <div className="App">
       {/* <CreateTeamTable /> */}
       {/* <LayiheDetallari /> */}
       {/* <ModalCreatTeamTable /> */}
-      <Card basliq='salam' buttonExist='true'/>
+      <Card basliq='Komandani yarat' buttonExist='true'/>
+      
     </div>
   );
 }
