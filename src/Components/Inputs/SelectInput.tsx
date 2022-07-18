1d815fb9e23c91e1b0bd028c569228e9bf13ad32
Asifdvp <asif.mustafayev.13@gmail.com>
2022-07-18 16:36:37 +0400

    Inputs Modified

--- src/Components/Inputs/SelectInput.tsx
+++ src/Components/Inputs/SelectInput.tsx
@@ -2,11 +2,6 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Arrow from '../../Assets/images/Arrow';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import './style.css'
 export default function SelectInput() {
  
--- src/Components/Inputs/StatusInput.tsx
+++ src/Components/Inputs/StatusInput.tsx
@@ -5,47 +5,46 @@ import Arrow from "../../Assets/images/Arrow";
 import { useRef } from "react";
 export default function StatusInput() {
   const inputRef: any = useRef();
-  const [labels, setLabels] = React.useState([
+  const [labels] = React.useState([
     { label: "Açıq" },
     { label: "İcradadır" },
     { label: "Gecikir" },
     { label: "Dayandırılıb" },
     { label: "Bağlı" },
   ]);
-  const changeColor = (event:any,value: any) => {
-if(value !== null){
-  switch (value.label) {
-    case "Açıq":
-      inputRef.current.style.backgroundColor = "#068FDB";
-      break;
-    case "İcradadır":
-      inputRef.current.style.backgroundColor = "yellow";
-      break;
-    case "Gecikir":
-      inputRef.current.style.backgroundColor = "teal";
-      break;
-    case "Dayandırılıb":
-      inputRef.current.style.backgroundColor = "red";
-      break;
-    case "Bağlı":
-      inputRef.current.style.backgroundColor = "#6FC03D";
-      break;
-      default :  inputRef.current.style.backgroundColor = "white";
-  }
-}
-else{
-  inputRef.current.style.backgroundColor = "white";
-}
+  const changeColor = (event: any, value: any) => {
+    if (value !== null) {
+      switch (value.label) {
+        case "Açıq":
+          inputRef.current.style.backgroundColor = "#068FDB";
+          break;
+        case "İcradadır":
+          inputRef.current.style.backgroundColor = "yellow";
+          break;
+        case "Gecikir":
+          inputRef.current.style.backgroundColor = "teal";
+          break;
+        case "Dayandırılıb":
+          inputRef.current.style.backgroundColor = "red";
+          break;
+        case "Bağlı":
+          inputRef.current.style.backgroundColor = "#6FC03D";
+          break;
+        default:
+          inputRef.current.style.backgroundColor = "white";
+      }
+    } else {
+      inputRef.current.style.backgroundColor = "white";
+    }
   };
   return (
     <Autocomplete
-    
-    disableClearable
+      disableClearable
       disablePortal
       fullWidth
       id="combo-box-demo"
       options={labels}
-      onChange={(event, value: any) => changeColor(event,value)}
+      onChange={(event, value: any) => changeColor(event, value)}
       popupIcon={<Arrow />}
       renderInput={(params) => (
         <TextField
